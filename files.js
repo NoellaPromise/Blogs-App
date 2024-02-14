80ef1c4d33f27bbfbf249d67d5307953bcab8fa5
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-14 17:00:00 +0200

    add blog1 text

--- files.js
+++ files.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 
 //reading files
-fs.readFile('./docs/blog.txt', (err,data)=>{
-if(err){
-    console.log(err);
-}
-console.log(data.toString());
-});
+// fs.readFile('./docs/blog.txt', (err,data)=>{
+// if(err){
+//     console.log(err);
+// }
+// console.log(data.toString());
+// });
+
+// //writing files
+// fs.writeFile('./docs/blog.txt','Hello again',()=>{
+// console.log('file was written')
+// });
+
+fs.writeFile('./docs/blog1.txt','Hello again',()=>{
+    console.log('file was written')
+    });

e4ee0d689e0e4df6ec6d5b974adee33bd185e71e
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-14 12:26:57 +0200

    create global file

---
+++ global.js
@@ -0,0 +1,10 @@
+global.setTimeout(() => {
+    console.log("In the timeout")
+}, 3000,);
+
+global.setInterval(() => {
+    console.log("welcome back to my youtube channel")
+}, 1000);
+
+console.log(__dirname);
+console.log(__filename);

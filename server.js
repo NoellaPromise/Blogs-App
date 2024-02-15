9fa9d56c179e4c56e14d52cd6582dcb1624c86d0
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-15 15:05:02 +0200

    create view folder

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http')
+const fs = require ('fs')
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method)
 
-    res.setHeader('content-Type','text/plain')
-res.write('Hello everyone')
-res.end()
+    res.setHeader('content-Type','text/html')
+fs.readFile('./veiws/index.html',(err,data)=>{
+    if(err){
+        console.log(err)
+        res.end()
+    }else{
+        res.end(data)
+
+    }
+})
 
 })
 server.listen(3000,'localhost',()=>{

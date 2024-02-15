2d2de2d6f306c791de59362aa3a42d33eb1752d7
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-15 15:49:03 +0200

    Redirect about-me page to about me

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.setHeader("content-Type", "text/html");
 
   let path = "./views/";
-  switch(req.url) {
+  switch (req.url) {
     case "/":
       path += "index.html";
       res.statusCode = 200;
@@ -14,6 +14,11 @@ const server = http.createServer((req, res) => {
     case "/about":
       path += "about.html";
       res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader('Location','/about')
+      res.end();
       break;
     default:
       path += "404.html";

e86581d49b9e500c46a6e80898ed6382c06d38fe
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-15 18:43:19 +0200

    add the nav bar

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express();
 app.listen(3000);
 app.get('/',(req,res)=>{
-    res.send('<p>Home page</>');
+    res.sendFile('./views/index.html',{root:__dirname});
 
 });
 app.get('/about',(req,res)=>{
-    res.send('<p>Welcome to the About page</>');
+    res.sendFile('./views/about.html',{root:__dirname});
 
 });

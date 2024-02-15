0c85d87cb3f0e4528ff6c2f4f1f0b3428c0c18a3
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-15 15:28:52 +0200

    implement routing

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-const http = require('http')
-const fs = require ('fs')
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method)
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
 
-    res.setHeader('content-Type','text/html')
-fs.readFile('./veiws/index.html',(err,data)=>{
-    if(err){
-        console.log(err)
-        res.end()
-    }else{
-        res.end(data)
+  res.setHeader("content-Type", "text/html");
 
+  let path = "./views/";
+  switch(req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
     }
-})
-
-})
-server.listen(3000,'localhost',()=>{
-    console.log('Listening to the request on port 3000')
-})
+  });
+});
+server.listen(3000, "localhost", () => {
+  console.log("Listening to the request on port 3000");
+});

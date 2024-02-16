7fc3e0bb3b00f0116a0da4040357cc046fbbef61
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-16 17:45:35 +0200

    find all blogs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require('./models/blog');
+const { result } = require("lodash");
 
 // express app
 const app = express();
@@ -32,6 +34,29 @@ app.use((req, res, next) => {
 });
 
 app.use(morgan("dev"));
+app.get('/add-blog',(req,res)=>{
+const blog = new Blog({
+    title:'new blog 2',
+    snippet:'about my new blog',
+    body : 'more about my new blog'
+});
+blog.save()
+.then((result)=>{
+    res.send(result)
+})
+.catch((err)=>{
+    console.log(err);
+})
+});
+app.get('/all-blogs',(req,res)=>{
+    Blog.find()
+    .then((result)=>{
+        res.send(result);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
 
 app.use((req, res, next) => {
   res.locals.path = req.path;

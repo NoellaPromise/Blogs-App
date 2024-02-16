bada8583cc060c4f63d30a1dcd7cb4dcdcb12bff
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-16 09:52:35 +0200

    create ejs files in view folder

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.listen(3000);
-app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
-
+app.get("/", (req, res) => {
+  res.render('index');
 });
-app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
-
+app.get("/about", (req, res) => {
+  res.render('about');
 });
-app.get('/about-me',(req,res)=>{
-   res.redirect('/about');
-
+app.get("/about-me", (req, res) => {
+  res.redirect("/about");
 });
-app.use((req,res)=>{
-res.status(404).sendFile('./views/404.html',{root:__dirname})
-})
+app.use((req, res) => {
+  res.status(404).render('404');
+});

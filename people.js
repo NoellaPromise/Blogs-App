b80e4095df2061b66ef8b16813c43bead6628ffa
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-02-22 11:43:42 +0200

    delete people and test files

--- people.js
+++
@@ -1,3 +0,0 @@
- const people = ["Noella","Papa","Mama"];
- console.log(people)
- module.exports = people;
--- test.js
+++
@@ -1,4 +0,0 @@
-const name = "Jesus";
-const boy = "male"
-console.log(name)
-console.log(boy)
